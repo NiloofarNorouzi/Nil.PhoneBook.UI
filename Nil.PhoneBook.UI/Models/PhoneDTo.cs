42d580e0d386ee60f8c40aa5f9182ddfd3b35eea
Niloufar Norouzi <n.norouzi@tajan.tech>
2021-08-22 13:44:06 +0430

    add phoneDTO

---
+++ Nil.PhoneBook.UI/Models/PhoneDTo.cs
@@ -0,0 +1,19 @@
+﻿using Nil.PhoneBook.UI.Data;
+using Nil.PhoneBook.UI.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nil.PhoneBook.UI.Models
+{
+    public class PhoneDTO
+    {
+        public int PhoneId { get; set; }
+        public int ContactId { get; set; }
+        public Contact Contact { get; set; }
+
+        public PhoneType PhoneType { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
